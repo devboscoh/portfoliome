5e6319466c3371633480029e9fbc41e28d2e0ed6
Boscoh722 <boscobrilli8@gmail.com>
2025-12-17 22:25:07 +0300

    Minimum viable portfolio

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const frontendPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(frontendPath));
 
 // React SPA fallback
-app.get('/*', (req, res) => {
+app.get('/.*/', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
